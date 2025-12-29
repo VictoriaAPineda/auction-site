7d681e4d13d0026c4594f8795d9fd66909ad1816
Victoria <piklad1996@gmail.com>
2025-12-29 13:53:52 -0600

    update
    updates

---
+++ app/items/page.tsx
@@ -0,0 +1,18 @@
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
+
+/* This page will display a gallery of items that will run in auction */
+export default function Items () {
+    // console.log(supabase) 
+    
+
+
+
+
+
+    return(
+        <>
+            <p>Gallery of Items here...</p>
+        </>
+    )  
+}
