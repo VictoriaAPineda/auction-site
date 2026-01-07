25d2976b1649dc918424496456ecfe3952ae9789
Victoria <piklad1996@gmail.com>
2026-01-07 12:38:16 -0600

    cleanup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,29 +4,6 @@ import Auth from "../components/auth";
 import supabase from "../config/supabaseClient";
 
 export default function Login () {
-    const [session, setSession] = useState<any>(null);
-
-// maake this to a context....
-    const fetchSession = async () =>{
-        const currentSession = await supabase.auth.getSession();
-        // supbase gets the sesssion (value : if user is logged in OR not)
-        setSession(currentSession);
-    }
-    useEffect(()=>{
-        fetchSession();
-        // supabase listens to event
-        // automactically switches states
-        const {data: authListener} = supabase.auth.onAuthStateChange(
-            (_event, session)=> {
-                setSession(session)
-            }
-        );
-        return() => {
-            // clean up after subscription to prevent 
-            // memnory leaks
-            authListener.subscription.unsubscribe();
-        }
-    },[])
 
     return(
         <>
